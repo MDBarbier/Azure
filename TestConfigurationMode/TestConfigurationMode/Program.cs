fbb8c583c83d670a0000c05cba1fe0b9a94f01a6
Matt Barbier <matt.barbier@os.uk>
2019-09-16 15:14:21 +0100

    Added configuration mode test project

---
+++ TestConfigurationMode/TestConfigurationMode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConfigurationMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+#if DEBUG
+            /* Debug mode logic - switch to Release mode in VS to get Intellisense! */
+
+            Console.WriteLine("Debug mode");
+
+#else
+            /* Release mode logic - switch to Release mode in VS to get Intellisense! */
+
+            Console.WriteLine("Debug mode");
+#endif
+
+
+        }
+    }
+}
