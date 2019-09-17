ff8fb95cc792f1d1f06746057d049d54e8e36381
Matt Barbier <matt.barbier@os.uk>
2019-09-17 07:33:28 +0100

    Azure unit test run mode test added

---
+++ AzureUnitTestReleaseModeTest/AzureUnitTestReleaseModeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AzureUnitTestReleaseModeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AzureUnitTestReleaseModeTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("58d7601c-3341-43c5-9c67-78fe1b221f5b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AzureUnitTestReleaseModeTest/AzureUnitTestReleaseModeTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AzureUnitTestReleaseModeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+#if DEBUG
+            Console.WriteLine("Running in debug");
+#else
+            Console.WriteLine("Running in release");
+#endif
+
+            Assert.IsTrue(1 < 2);
+        }
+    }
+}
