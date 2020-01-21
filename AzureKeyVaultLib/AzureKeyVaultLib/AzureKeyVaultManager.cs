ac820562e0154a8c40f890b8a6d782693f773bba
Matt Barbier <matthewdavidbarbier@gmail.com>
2020-01-21 20:58:52 +0000

    Initial commit of class library that can interact with an Azure Key Vault

---
+++ AzureKeyVaultLib/AzureKeyVaultLib/AzureKeyVaultManager.cs
@@ -0,0 +1,75 @@
+﻿using Azure;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System;
+using System.Collections.Generic;
+
+namespace AzureKeyVaultLib
+{
+    public class AzureKeyVaultManager
+    {
+        internal string KeyVaultUrl { get; set; }
+        internal SecretClient SecretClient { get; set; }
+
+        /// <summary>
+        /// Constructor sets up the SecretClient using DefaultAzureCredentials object (which reads clientId, clientSecret and tenantId from environment variables, see ReadMe.md)
+        /// </summary>
+        /// <param name="vaultName">The name of the vault to open</param>
+        public AzureKeyVaultManager(string vaultName)
+        {
+            KeyVaultUrl = vaultName;
+            SecretClient = new SecretClient(vaultUri: new Uri(KeyVaultUrl), credential: new DefaultAzureCredential());
+        }
+
+        /// <summary>
+        /// Add a secret to the vault
+        /// </summary>
+        /// <param name="secretName">The name to give the secret</param>
+        /// <param name="secretValue">The value the secret should have</param>
+        /// <returns>True if succesfully added</returns>
+        public bool AddSecret(string secretName, string secretValue)
+        {            
+            var secret = SecretClient.SetSecret(secretName, secretValue);
+
+            return string.IsNullOrEmpty(secret.Value.Value) ? false : true;            
+        }
+
+        /// <summary>
+        /// Get the value of the specified secret
+        /// </summary>
+        /// <param name="secretName">The secret to get the value of</param>
+        /// <returns>String value of the secret</returns>
+        public string GetSecretValue(string secretName)
+        {
+            return SecretClient.GetSecret(secretName).Value.Value;
+        }
+
+        /// <summary>
+        /// Get the specified secret object
+        /// </summary>
+        /// <param name="secretName">The secret to get</param>
+        /// <returns>KeyVaultSecret object</returns>
+        public KeyVaultSecret GetSecret(string secretName)
+        {
+            return SecretClient.GetSecret(secretName).Value;
+        }
+
+        /// <summary>
+        /// Return an enumerable containing the properties of every secret in the vault i.e. Name etc. Note that the value is not included you must call the GetSecretValue method.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<SecretProperties> GetAllSecretProperties()
+        {
+            var secretsList = new List<SecretProperties>();
+
+            Pageable<SecretProperties> allSecrets = SecretClient.GetPropertiesOfSecrets();
+
+            foreach (SecretProperties secretProperties in allSecrets)
+            {
+                secretsList.Add(secretProperties);
+            }
+
+            return secretsList;
+        }
+    }
+}
---
+++ AzureKeyVaultLib/AzureKeyVaultLib/ReadMe.md
@@ -0,0 +1,16 @@
+﻿Note the "new DefaultAzureCredential()" line of code relies on three environment variables:
+
+AZURE_CLIENT_SECRET
+AZURE_CLIENT_ID
+AZURE_TENANT_ID
+
+These can be set in powershell like this:
+
+[System.Environment]::SetEnvironmentVariable('AZURE_TENANT_ID','xxxx-xxxxxxxx-xxxxxxxxxx-xxxx',[System.EnvironmentVariableTarget]::User)
+
+
+--------
+
+
+
+There must be a Service Principal, such as that linked to an App Registration, which has permissions on the KeyVault. Note this is assigned in the "Acces Policies" screen and not the IAM Access Control screen.
